9d21d5102d317886a7b3cfab7c6bd95704b6ca6e
Rus7Iv <rusivary@gmail.com>
2024-03-11 15:07:12 +0400

    added empty response handler

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -1,27 +1,33 @@
 import { IProductTableProps } from './interface'
 import './style.css'
 
-const ProductTable =({ products }: IProductTableProps) => (
-  <table className="product-table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Product</th>
-        <th>Brand</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      {products.map(product => (
-        <tr key={product.id}>
-          <td>{product.id || '-'}</td>
-          <td>{product.product || '-'}</td>
-          <td>{product.brand || '-'}</td>
-          <td>{product.price !== undefined ? product.price : '-'}</td>
+const ProductTable =({ products }: IProductTableProps) => {
+  if (products.length === 0) {
+    return <h1>Not found</h1>
+  }
+
+  return (
+    <table className="product-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Product</th>
+          <th>Brand</th>
+          <th>Price</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {products.map(product => (
+          <tr key={product.id}>
+            <td>{product.id || '-'}</td>
+            <td>{product.product || '-'}</td>
+            <td>{product.brand || '-'}</td>
+            <td>{product.price !== undefined ? product.price : '-'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default ProductTable;
